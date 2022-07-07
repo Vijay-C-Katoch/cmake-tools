3ae1735a7fa5ef569904729a23ebe36aa0efa5ab
Vijay-C-Katoch <katoch.vijay85@gmail.com>
2022-07-07 16:56:39 +0200

    cmake updated

--- boards/stm32/NUCLEO-L476RG.cmake
+++ boards/stm32/NUCLEO-L476RG.cmake
@@ -5,129 +5,99 @@ target_compile_options(cubeL4 PUBLIC
   )
 
 target_include_directories(cubeL4 PUBLIC
-  $<BUILD_INTERFACE:${OSAL_SOURCE_DIR}/${CORE_DIR}/Inc>
   $<INSTALL_INTERFACE:bsp/Inc>
 
-  ${CUBE_DIR}/Middlewares/Third_Party/FreeRTOS/Source/include
-  ${CUBE_DIR}/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM7/r0p1
-  ${CUBE_DIR}/Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2
+  ${STM32_DIR}/Middlewares/Third_Party/FreeRTOS/Source/include
+  ${STM32_DIR}/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM7/r0p1
+  ${STM32_DIR}/Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2
 
-  ${CUBE_DIR}/Drivers/CMSIS/Device/ST/STM32L4xx/Include
-  ${CUBE_DIR}/Drivers/CMSIS/Include
-  ${CUBE_DIR}/Drivers/STM32L4xx_HAL_Driver/Inc
+  ${STM32_DIR}/Drivers/CMSIS/Device/ST/STM32L4xx/Include
+  ${STM32_DIR}/Drivers/CMSIS/Include
+  ${STM32_DIR}/Drivers/STM32L4xx_HAL_Driver/Inc
 
-  ${CUBE_DIR}/Middlewares/Third_Party/LwIP/src/include
-  ${CUBE_DIR}/Middlewares/Third_Party/LwIP/system
+  ${STM32_DIR}/Middlewares/Third_Party/LwIP/src/include
+  ${STM32_DIR}/Middlewares/Third_Party/LwIP/system
 
-  ${CUBE_DIR}/Middlewares/Third_Party/FatFs/src
+  ${STM32_DIR}/Middlewares/Third_Party/FatFs/src
   )
 
 target_include_directories(cubeL4 PRIVATE
-  ${CUBE_DIR}/Middlewares/Third_Party/LwIP/src/include/compat/posix
-  ${CUBE_DIR}/Middlewares/Third_Party/LwIP/src/include/compat/posix/arpa
-  ${CUBE_DIR}/Middlewares/Third_Party/LwIP/src/include/compat/posix/net
-  ${CUBE_DIR}/Middlewares/Third_Party/LwIP/src/include/compat/posix/sys
-  ${CUBE_DIR}/Middlewares/Third_Party/LwIP/src/include/compat/stdc
-  ${CUBE_DIR}/Middlewares/Third_Party/LwIP/src/include/netif
-  ${CUBE_DIR}/Middlewares/Third_Party/LwIP/system/arch
+  ${STM32_DIR}/Middlewares/Third_Party/LwIP/src/include/compat/posix
+  ${STM32_DIR}/Middlewares/Third_Party/LwIP/src/include/compat/posix/arpa
+  ${STM32_DIR}/Middlewares/Third_Party/LwIP/src/include/compat/posix/net
+  ${STM32_DIR}/Middlewares/Third_Party/LwIP/src/include/compat/posix/sys
+  ${STM32_DIR}/Middlewares/Third_Party/LwIP/src/include/compat/stdc
+  ${STM32_DIR}/Middlewares/Third_Party/LwIP/src/include/netif
+  ${STM32_DIR}/Middlewares/Third_Party/LwIP/system/arch
   )
 
-target_sources(cubeL4 PRIVATE
-  ${CORE_DIR}/Src/bsp_driver_sd.c
-  ${CORE_DIR}/Src/ethernetif.c
-  ${CORE_DIR}/Src/fatfs.c
-  ${CORE_DIR}/Src/fatfs_platform.c
-  ${CORE_DIR}/Src/lwip.c
-  ${CORE_DIR}/Src/sd_diskio.c
 
-  ${CUBE_DIR}/Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2/cmsis_os2.c
-  ${CUBE_DIR}/Middlewares/Third_Party/FreeRTOS/Source/event_groups.c
-  ${CUBE_DIR}/Middlewares/Third_Party/FreeRTOS/Source/list.c
-  ${CUBE_DIR}/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM7/r0p1/port.c
-  ${CUBE_DIR}/Middlewares/Third_Party/FreeRTOS/Source/portable/MemMang/heap_4.c
-  ${CUBE_DIR}/Middlewares/Third_Party/FreeRTOS/Source/queue.c
-  ${CUBE_DIR}/Middlewares/Third_Party/FreeRTOS/Source/tasks.c
-  ${CUBE_DIR}/Middlewares/Third_Party/FreeRTOS/Source/timers.c
+  ${STM32_DIR}/Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2/cmsis_os2.c
+  ${STM32_DIR}/Middlewares/Third_Party/FreeRTOS/Source/event_groups.c
+  ${STM32_DIR}/Middlewares/Third_Party/FreeRTOS/Source/list.c
+  ${STM32_DIR}/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM7/r0p1/port.c
+  ${STM32_DIR}/Middlewares/Third_Party/FreeRTOS/Source/portable/MemMang/heap_4.c
+  ${STM32_DIR}/Middlewares/Third_Party/FreeRTOS/Source/queue.c
+  ${STM32_DIR}/Middlewares/Third_Party/FreeRTOS/Source/tasks.c
+  ${STM32_DIR}/Middlewares/Third_Party/FreeRTOS/Source/timers.c
 
-  ${CUBE_DIR}/Middlewares/Third_Party/LwIP/src/api/api_lib.c
-  ${CUBE_DIR}/Middlewares/Third_Party/LwIP/src/api/api_msg.c
-  ${CUBE_DIR}/Middlewares/Third_Party/LwIP/src/api/err.c
-  ${CUBE_DIR}/Middlewares/Third_Party/LwIP/src/api/if_api.c
-  ${CUBE_DIR}/Middlewares/Third_Party/LwIP/src/api/netbuf.c
-  ${CUBE_DIR}/Middlewares/Third_Party/LwIP/src/api/netdb.c
-  ${CUBE_DIR}/Middlewares/Third_Party/LwIP/src/api/netifapi.c
-  ${CUBE_DIR}/Middlewares/Third_Party/LwIP/src/api/sockets.c
-  ${CUBE_DIR}/Middlewares/Third_Party/LwIP/src/api/tcpip.c
-  ${CUBE_DIR}/Middlewares/Third_Party/LwIP/src/core/def.c
-  ${CUBE_DIR}/Middlewares/Third_Party/LwIP/src/core/init.c
-  ${CUBE_DIR}/Middlewares/Third_Party/LwIP/src/core/ip.c
-  ${CUBE_DIR}/Middlewares/Third_Party/LwIP/src/core/ipv4/dhcp.c
-  ${CUBE_DIR}/Middlewares/Third_Party/LwIP/src/core/ipv4/etharp.c
-  ${CUBE_DIR}/Middlewares/Third_Party/LwIP/src/core/ipv4/icmp.c
-  ${CUBE_DIR}/Middlewares/Third_Party/LwIP/src/core/ipv4/ip4.c
-  ${CUBE_DIR}/Middlewares/Third_Party/LwIP/src/core/ipv4/ip4_addr.c
-  ${CUBE_DIR}/Middlewares/Third_Party/LwIP/src/core/ipv4/ip4_frag.c
-  ${CUBE_DIR}/Middlewares/Third_Party/LwIP/src/core/mem.c
-  ${CUBE_DIR}/Middlewares/Third_Party/LwIP/src/core/memp.c
-  ${CUBE_DIR}/Middlewares/Third_Party/LwIP/src/core/netif.c
-  ${CUBE_DIR}/Middlewares/Third_Party/LwIP/src/core/pbuf.c
-  ${CUBE_DIR}/Middlewares/Third_Party/LwIP/src/core/stats.c
-  ${CUBE_DIR}/Middlewares/Third_Party/LwIP/src/core/tcp.c
-  ${CUBE_DIR}/Middlewares/Third_Party/LwIP/src/core/tcp_in.c
-  ${CUBE_DIR}/Middlewares/Third_Party/LwIP/src/core/tcp_out.c
-  ${CUBE_DIR}/Middlewares/Third_Party/LwIP/src/core/timeouts.c
-  ${CUBE_DIR}/Middlewares/Third_Party/LwIP/src/core/udp.c
-  ${CUBE_DIR}/Middlewares/Third_Party/LwIP/src/netif/ethernet.c
-  ${CUBE_DIR}/Middlewares/Third_Party/LwIP/system/OS/sys_arch.c
+  ${STM32_DIR}/Middlewares/Third_Party/LwIP/src/api/api_lib.c
+  ${STM32_DIR}/Middlewares/Third_Party/LwIP/src/api/api_msg.c
+  ${STM32_DIR}/Middlewares/Third_Party/LwIP/src/api/err.c
+  ${STM32_DIR}/Middlewares/Third_Party/LwIP/src/api/if_api.c
+  ${STM32_DIR}/Middlewares/Third_Party/LwIP/src/api/netbuf.c
+  ${STM32_DIR}/Middlewares/Third_Party/LwIP/src/api/netdb.c
+  ${STM32_DIR}/Middlewares/Third_Party/LwIP/src/api/netifapi.c
+  ${STM32_DIR}/Middlewares/Third_Party/LwIP/src/api/sockets.c
+  ${STM32_DIR}/Middlewares/Third_Party/LwIP/src/api/tcpip.c
+  ${STM32_DIR}/Middlewares/Third_Party/LwIP/src/core/def.c
+  ${STM32_DIR}/Middlewares/Third_Party/LwIP/src/core/init.c
+  ${STM32_DIR}/Middlewares/Third_Party/LwIP/src/core/ip.c
+  ${STM32_DIR}/Middlewares/Third_Party/LwIP/src/core/ipv4/dhcp.c
+  ${STM32_DIR}/Middlewares/Third_Party/LwIP/src/core/ipv4/etharp.c
+  ${STM32_DIR}/Middlewares/Third_Party/LwIP/src/core/ipv4/icmp.c
+  ${STM32_DIR}/Middlewares/Third_Party/LwIP/src/core/ipv4/ip4.c
+  ${STM32_DIR}/Middlewares/Third_Party/LwIP/src/core/ipv4/ip4_addr.c
+  ${STM32_DIR}/Middlewares/Third_Party/LwIP/src/core/ipv4/ip4_frag.c
+  ${STM32_DIR}/Middlewares/Third_Party/LwIP/src/core/mem.c
+  ${STM32_DIR}/Middlewares/Third_Party/LwIP/src/core/memp.c
+  ${STM32_DIR}/Middlewares/Third_Party/LwIP/src/core/netif.c
+  ${STM32_DIR}/Middlewares/Third_Party/LwIP/src/core/pbuf.c
+  ${STM32_DIR}/Middlewares/Third_Party/LwIP/src/core/stats.c
+  ${STM32_DIR}/Middlewares/Third_Party/LwIP/src/core/tcp.c
+  ${STM32_DIR}/Middlewares/Third_Party/LwIP/src/core/tcp_in.c
+  ${STM32_DIR}/Middlewares/Third_Party/LwIP/src/core/tcp_out.c
+  ${STM32_DIR}/Middlewares/Third_Party/LwIP/src/core/timeouts.c
+  ${STM32_DIR}/Middlewares/Third_Party/LwIP/src/core/udp.c
+  ${STM32_DIR}/Middlewares/Third_Party/LwIP/src/netif/ethernet.c
+  ${STM32_DIR}/Middlewares/Third_Party/LwIP/system/OS/sys_arch.c
 
-  ${CUBE_DIR}/Middlewares/Third_Party/FatFs/src/diskio.c
-  ${CUBE_DIR}/Middlewares/Third_Party/FatFs/src/ff.c
-  ${CUBE_DIR}/Middlewares/Third_Party/FatFs/src/ff_gen_drv.c
-  ${CUBE_DIR}/Middlewares/Third_Party/FatFs/src/ff_gen_drv.c
-  ${CUBE_DIR}/Middlewares/Third_Party/FatFs/src/option/ccsbcs.c
-  ${CUBE_DIR}/Middlewares/Third_Party/FatFs/src/option/syscall.c
+  ${STM32_DIR}/Middlewares/Third_Party/FatFs/src/diskio.c
+  ${STM32_DIR}/Middlewares/Third_Party/FatFs/src/ff.c
+  ${STM32_DIR}/Middlewares/Third_Party/FatFs/src/ff_gen_drv.c
+  ${STM32_DIR}/Middlewares/Third_Party/FatFs/src/ff_gen_drv.c
+  ${STM32_DIR}/Middlewares/Third_Party/FatFs/src/option/ccsbcs.c
+  ${STM32_DIR}/Middlewares/Third_Party/FatFs/src/option/syscall.c
 
-  ${CUBE_DIR}/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal.c
-  ${CUBE_DIR}/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_cortex.c
-  ${CUBE_DIR}/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dma.c
-  ${CUBE_DIR}/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_eth.c
-  ${CUBE_DIR}/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_gpio.c
-  ${CUBE_DIR}/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_pwr_ex.c
-  ${CUBE_DIR}/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_rcc.c
-  ${CUBE_DIR}/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_rcc_ex.c
-  ${CUBE_DIR}/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_sd.c
-  ${CUBE_DIR}/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_tim.c
-  ${CUBE_DIR}/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_tim_ex.c
-  ${CUBE_DIR}/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_uart.c
-  ${CUBE_DIR}/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_ll_sdmmc.c
+  ${STM32_DIR}/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal.c
+  ${STM32_DIR}/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_cortex.c
+  ${STM32_DIR}/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dma.c
+  ${STM32_DIR}/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_eth.c
+  ${STM32_DIR}/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_gpio.c
+  ${STM32_DIR}/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_pwr_ex.c
+  ${STM32_DIR}/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_rcc.c
+  ${STM32_DIR}/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_rcc_ex.c
+  ${STM32_DIR}/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_sd.c
+  ${STM32_DIR}/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_tim.c
+  ${STM32_DIR}/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_tim_ex.c
+  ${STM32_DIR}/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_uart.c
+  ${STM32_DIR}/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_ll_sdmmc.c
   )
 
-add_library(cubeL4-bsp INTERFACE)
-target_sources(cubeL4-bsp INTERFACE
-  $<BUILD_INTERFACE:${OSAL_SOURCE_DIR}/${CORE_DIR}/Src/freertos.c>
-  $<BUILD_INTERFACE:${OSAL_SOURCE_DIR}/${CORE_DIR}/Src/main.c>
-  $<BUILD_INTERFACE:${OSAL_SOURCE_DIR}/${CORE_DIR}/Src/startup_stm32f769xx.s>
-  $<BUILD_INTERFACE:${OSAL_SOURCE_DIR}/${CORE_DIR}/Src/stm32l4xx_hal_msp.c>
-  $<BUILD_INTERFACE:${OSAL_SOURCE_DIR}/${CORE_DIR}/Src/stm32l4xx_hal_timebase_tim.c>
-  $<BUILD_INTERFACE:${OSAL_SOURCE_DIR}/${CORE_DIR}/Src/stm32l4xx_it.c>
-  $<BUILD_INTERFACE:${OSAL_SOURCE_DIR}/${CORE_DIR}/Src/stubs.c>
-  $<BUILD_INTERFACE:${OSAL_SOURCE_DIR}/${CORE_DIR}/Src/syscalls.c>
-  $<BUILD_INTERFACE:${OSAL_SOURCE_DIR}/${CORE_DIR}/Src/system_stm32f7xx.c>
-
-  $<INSTALL_INTERFACE:bsp/Src/freertos.c>
-  $<INSTALL_INTERFACE:bsp/Src/main.c>
-  $<INSTALL_INTERFACE:bsp/Src/startup_stm32f769xx.s>
-  $<INSTALL_INTERFACE:bsp/Src/stm32l4xx_hal_msp.c>
-  $<INSTALL_INTERFACE:bsp/Src/stm32l4xx_hal_timebase_tim.c>
-  $<INSTALL_INTERFACE:bsp/Src/stm32l4xx_it.c>
-  $<INSTALL_INTERFACE:bsp/Src/stubs.c>
-  $<INSTALL_INTERFACE:bsp/Src/syscalls.c>
-  $<INSTALL_INTERFACE:bsp/Src/system_stm32f7xx.c>
-  )
 target_link_options(cubeL4-bsp INTERFACE
   -specs=nano.specs
-  -T${OSAL_SOURCE_DIR}/${CORE_DIR}/STM32F769NIHx_FLASH.ld
+  -T${HARDWARE_DIR}/boards/stm32/STM32L476RGTX_FLASH.ld
   )
 
-install(DIRECTORY ${CORE_DIR}/Src DESTINATION bsp)
-install(DIRECTORY ${CORE_DIR}/Inc DESTINATION bsp)
+install(DIRECTORY ${HARDWARE_DIR}/boards/stm32/Src DESTINATION bsp)
+install(DIRECTORY ${HARDWARE_DIR}/boards/stm32/Inc DESTINATION bsp)
