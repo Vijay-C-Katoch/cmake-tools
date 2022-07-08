acc51b80bf447ca4f5fee0cf52c5ace7f39ffa3d
Vijay-C-Katoch <katoch.vijay85@gmail.com>
2022-07-09 01:23:29 +0200

    cmake corrected

--- boards/stm32/NUCLEO-L476RG.cmake
+++ boards/stm32/NUCLEO-L476RG.cmake
@@ -1,5 +1,8 @@
 
+add_library(cubeL4 STATIC)
+
 target_compile_options(cubeL4 PUBLIC
+-DDEBUG
 -DUSE_HAL_DRIVER
 -DSTM32L476xx
 )
@@ -15,7 +18,7 @@ ${STM32_DIR}/Drivers/STM32L4xx_HAL_Driver/Inc
 ${STM32_DIR}/Middlewares/Third_Party/FatFs/src
 )
 
-target_sources(cubeL4 PRIVATE
+target_sources(cubeL4 PUBLIC
 ${HARDWARE_DIR}/boards/stm32/NUCLEO-L476RG/Src/board_init.c
 ${HARDWARE_DIR}/boards/stm32/NUCLEO-L476RG/Src/fatfs.c
 ${HARDWARE_DIR}/boards/stm32/NUCLEO-L476RG/Src/startup_stm32l476rgtx.s
@@ -32,46 +35,31 @@ ${STM32_DIR}/Middlewares/Third_Party/FatFs/src/ff_gen_drv.c
 ${STM32_DIR}/Middlewares/Third_Party/FatFs/src/ff_gen_drv.c
 ${STM32_DIR}/Middlewares/Third_Party/FatFs/src/option/syscall.c
 
-${STM32_DIR}/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal.c
 ${STM32_DIR}/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_cortex.c
+${STM32_DIR}/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dma_ex.c
 ${STM32_DIR}/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dma.c
+${STM32_DIR}/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_exti.c
+${STM32_DIR}/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash_ex.c
+${STM32_DIR}/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash_ramfunc.c
+${STM32_DIR}/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash.c
 ${STM32_DIR}/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_gpio.c
+${STM32_DIR}/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_i2c_ex.c
+${STM32_DIR}/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_i2c.c
 ${STM32_DIR}/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_pwr_ex.c
 ${STM32_DIR}/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_rcc.c
 ${STM32_DIR}/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_rcc_ex.c
 ${STM32_DIR}/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_sd.c
 ${STM32_DIR}/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_tim.c
 ${STM32_DIR}/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_tim_ex.c
+${STM32_DIR}/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_uart_ex.c
 ${STM32_DIR}/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_uart.c
+${STM32_DIR}/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal.c
 ${STM32_DIR}/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_ll_sdmmc.c
 )
 
-add_library(cubeL4-bsp INTERFACE)
-target_sources(cubeL4-bsp INTERFACE
-  $<BUILD_INTERFACE:${HARDWARE_DIR}/boards/stm32/NUCLEO-L476RG/Src/board_init.c>
-  $<BUILD_INTERFACE:${HARDWARE_DIR}/boards/stm32/NUCLEO-L476RG/Src/startup_stm32l476rgtx.s>
-  $<BUILD_INTERFACE:${HARDWARE_DIR}/boards/stm32/NUCLEO-L476RG/Src/stm32l4xx_hal_msp.c>
-  $<BUILD_INTERFACE:${HARDWARE_DIR}/boards/stm32/NUCLEO-L476RG/Src/stm32l4xx_it.c>
-  $<BUILD_INTERFACE:${HARDWARE_DIR}/boards/stm32/NUCLEO-L476RG/Src/syscalls.c>
-  $<BUILD_INTERFACE:${HARDWARE_DIR}/boards/stm32/NUCLEO-L476RG/Src/sysmem.c>
-  $<BUILD_INTERFACE:${HARDWARE_DIR}/boards/stm32/NUCLEO-L476RG/Src/system_stm32l4xx.c>
-
-  $<INSTALL_INTERFACE:bsp/Src/board_init.c>
-  $<INSTALL_INTERFACE:bsp/Src/startup_stm32l476rgtx.s>
-  $<INSTALL_INTERFACE:bsp/Src/stm32l4xx_hal_msp.c>
-  $<INSTALL_INTERFACE:bsp/Src/stm32l4xx_it.c>
-  $<INSTALL_INTERFACE:bsp/Src/syscalls.c>
-  $<INSTALL_INTERFACE:bsp/Src/sysmem.c>
-  $<INSTALL_INTERFACE:bsp/Src/system_stm32l4xx.c>
-  )
-
-target_link_options(cubeL4 INTERFACE
+target_link_options(cubeL4 PUBLIC
 -specs=nano.specs
 -specs=nosys.specs
 -T${HARDWARE_DIR}/boards/stm32/NUCLEO-L476RG/STM32L476RGTX_FLASH.ld
 )
-target_link_libraries(cubeL4-bsp INTERFACE cubeL4)
 
-install(TARGETS cubeL4-bsp)
-install(DIRECTORY ${HARDWARE_DIR}/boards/stm32/NUCLEO-L476RG/Src DESTINATION bsp)
-install(DIRECTORY ${HARDWARE_DIR}/boards/stm32/NUCLEO-L476RG/Inc DESTINATION bsp)
