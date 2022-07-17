015ba742361b4af288c597e0ba26a52ecba365f5
Vijay-C-Katoch <katoch.vijay85@gmail.com>
2022-07-17 14:25:48 +0200

    build files for stm32duino

--- boards/stm32/NUCLEO-L476RG.cmake
+++ boards/stm32/NUCLEO-L476RG.cmake
@@ -21,11 +21,11 @@ target_include_directories(cubeL4 PUBLIC
 $<BUILD_INTERFACE:${HARDWARE_DIR}/boards/stm32/NUCLEO-L476RG/Inc>
 $<INSTALL_INTERFACE:bsp/Inc>
 
-${STM32_DIR}/Drivers/CMSIS/Device/ST/STM32L4xx/Include
-${STM32_DIR}/Drivers/CMSIS/Include
-${STM32_DIR}/Drivers/STM32L4xx_HAL_Driver/Inc
+${STM32_DIR}/STM32CubeL4/Drivers/CMSIS/Device/ST/STM32L4xx/Include
+${STM32_DIR}/STM32CubeL4/Drivers/CMSIS/Include
+${STM32_DIR}/STM32CubeL4/Drivers/STM32L4xx_HAL_Driver/Inc
 
-${STM32_DIR}/Middlewares/Third_Party/FatFs/src
+${STM32_DIR}/STM32CubeL4/Middlewares/Third_Party/FatFs/src
 )
 
 target_sources(cubeL4 PUBLIC
@@ -41,30 +41,30 @@ ${HARDWARE_DIR}/boards/stm32/NUCLEO-L476RG/Src/system_stm32l4xx.c
 ${HARDWARE_DIR}/boards/stm32/NUCLEO-L476RG/Src/usart.c
 ${HARDWARE_DIR}/boards/stm32/NUCLEO-L476RG/Src/user_diskio.c
 
-${STM32_DIR}/Middlewares/Third_Party/FatFs/src/diskio.c
-${STM32_DIR}/Middlewares/Third_Party/FatFs/src/ff.c
-${STM32_DIR}/Middlewares/Third_Party/FatFs/src/ff_gen_drv.c
-${STM32_DIR}/Middlewares/Third_Party/FatFs/src/ff_gen_drv.c
-${STM32_DIR}/Middlewares/Third_Party/FatFs/src/option/syscall.c
+${STM32_DIR}/STM32CubeL4/Middlewares/Third_Party/FatFs/src/diskio.c
+${STM32_DIR}/STM32CubeL4/Middlewares/Third_Party/FatFs/src/ff.c
+${STM32_DIR}/STM32CubeL4/Middlewares/Third_Party/FatFs/src/ff_gen_drv.c
+${STM32_DIR}/STM32CubeL4/Middlewares/Third_Party/FatFs/src/ff_gen_drv.c
+${STM32_DIR}/STM32CubeL4/Middlewares/Third_Party/FatFs/src/option/syscall.c
 
-${STM32_DIR}/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_cortex.c
-${STM32_DIR}/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dma_ex.c
-${STM32_DIR}/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dma.c
-${STM32_DIR}/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_exti.c
-${STM32_DIR}/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash_ex.c
-${STM32_DIR}/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash_ramfunc.c
-${STM32_DIR}/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash.c
-${STM32_DIR}/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_gpio.c
-${STM32_DIR}/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_i2c_ex.c
-${STM32_DIR}/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_i2c.c
-${STM32_DIR}/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_pwr_ex.c
-${STM32_DIR}/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_rcc.c
-${STM32_DIR}/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_rcc_ex.c
-${STM32_DIR}/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_sd.c
-${STM32_DIR}/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_tim.c
-${STM32_DIR}/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_tim_ex.c
-${STM32_DIR}/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_uart_ex.c
-${STM32_DIR}/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_uart.c
-${STM32_DIR}/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal.c
-${STM32_DIR}/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_ll_sdmmc.c
+${STM32_DIR}/STM32CubeL4/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_cortex.c
+${STM32_DIR}/STM32CubeL4/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dma_ex.c
+${STM32_DIR}/STM32CubeL4/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dma.c
+${STM32_DIR}/STM32CubeL4/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_exti.c
+${STM32_DIR}/STM32CubeL4/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash_ex.c
+${STM32_DIR}/STM32CubeL4/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash_ramfunc.c
+${STM32_DIR}/STM32CubeL4/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash.c
+${STM32_DIR}/STM32CubeL4/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_gpio.c
+${STM32_DIR}/STM32CubeL4/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_i2c_ex.c
+${STM32_DIR}/STM32CubeL4/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_i2c.c
+${STM32_DIR}/STM32CubeL4/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_pwr_ex.c
+${STM32_DIR}/STM32CubeL4/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_rcc.c
+${STM32_DIR}/STM32CubeL4/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_rcc_ex.c
+${STM32_DIR}/STM32CubeL4/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_sd.c
+${STM32_DIR}/STM32CubeL4/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_tim.c
+${STM32_DIR}/STM32CubeL4/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_tim_ex.c
+${STM32_DIR}/STM32CubeL4/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_uart_ex.c
+${STM32_DIR}/STM32CubeL4/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_uart.c
+${STM32_DIR}/STM32CubeL4/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal.c
+${STM32_DIR}/STM32CubeL4/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_ll_sdmmc.c
 )
