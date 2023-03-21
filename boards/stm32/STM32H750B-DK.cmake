5ec6b8e889d3036464e1fe66b78efe89d1d6c298
Vijay Katoch <katoch.vijay85@gmail.com>
2023-03-21 19:49:17 +0100

    Add board source location

--- boards/stm32/STM32H750B-DK.cmake
+++ boards/stm32/STM32H750B-DK.cmake
@@ -10,7 +10,7 @@ board from ST Microelectronics.
 
 add_library(framework-stm32cubeh7 STATIC)
 
-target_compile_options(cubeL4 PUBLIC
+target_compile_options(framework-stm32cubeh7 PUBLIC
 -DDEBUG
 -DUSE_HAL_DRIVER
 -DSTM32H7xx
@@ -104,3 +104,55 @@ ${TP_DIR}/framework-stm32cubeh7/Middlewares/ST/STM32_USB_Host_Library/Class/MTP/
 ${TP_DIR}/framework-stm32cubeh7/Middlewares/ST/STM32_USB_Host_Library/Class/MTP/Src
 )
 
+file(GLOB_RECURSE SOURCE_FILES CONFIGURE_DEPENDS 
+"${HARDWARE_DIR}/boards/stm32/STM32H750B-DK/Startup/startup_stm32h750xbhx.s"
+
+"${TP_DIR}/framework-stm32cubeh7/Drivers/STM32H7xx_HAL_Driver/Src/*.c"
+
+"${TP_DIR}/framework-stm32cubeh7/Drivers/BSP/Components/s70kl1281/*.c"
+"${TP_DIR}/framework-stm32cubeh7/Drivers/BSP/Components/rk070er9427/*.c"
+"${TP_DIR}/framework-stm32cubeh7/Drivers/BSP/Components/st7735/*.c"
+"${TP_DIR}/framework-stm32cubeh7/Drivers/BSP/Components/ts3510/*.c"
+"${TP_DIR}/framework-stm32cubeh7/Drivers/BSP/Components/Common/*.c"
+"${TP_DIR}/framework-stm32cubeh7/Drivers/BSP/Components/lan8742/*.c"
+"${TP_DIR}/framework-stm32cubeh7/Drivers/BSP/Components/ampire480272/*.c"
+"${TP_DIR}/framework-stm32cubeh7/Drivers/BSP/Components/m24lr64/*.c"
+"${TP_DIR}/framework-stm32cubeh7/Drivers/BSP/Components/ft6x06/*.c"
+"${TP_DIR}/framework-stm32cubeh7/Drivers/BSP/Components/is42s32800g/*.c"
+"${TP_DIR}/framework-stm32cubeh7/Drivers/BSP/Components/mt48lc4m32b2/*.c"
+"${TP_DIR}/framework-stm32cubeh7/Drivers/BSP/Components/is42s16800j/*.c"
+"${TP_DIR}/framework-stm32cubeh7/Drivers/BSP/Components/ampire640480/*.c"
+"${TP_DIR}/framework-stm32cubeh7/Drivers/BSP/Components/ov5640/*.c"
+"${TP_DIR}/framework-stm32cubeh7/Drivers/BSP/Components/cs42l51/*.c"
+"${TP_DIR}/framework-stm32cubeh7/Drivers/BSP/Components/otm8009a/*.c"
+"${TP_DIR}/framework-stm32cubeh7/Drivers/BSP/Components/es_wifi/*.c"
+"${TP_DIR}/framework-stm32cubeh7/Drivers/BSP/Components/mfxstm32l152/*.c"
+"${TP_DIR}/framework-stm32cubeh7/Drivers/BSP/Components/mx25lm51245g/*.c"
+"${TP_DIR}/framework-stm32cubeh7/Drivers/BSP/Components/mx_wifi/*.c"
+"${TP_DIR}/framework-stm32cubeh7/Drivers/BSP/Components/mt25tl01g/*.c"
+"${TP_DIR}/framework-stm32cubeh7/Drivers/BSP/Components/rk043fn48h/*.c"
+"${TP_DIR}/framework-stm32cubeh7/Drivers/BSP/Components/s5k5cag/*.c"
+"${TP_DIR}/framework-stm32cubeh7/Drivers/BSP/Components/adv7533/*.c"
+"${TP_DIR}/framework-stm32cubeh7/Drivers/BSP/Components/ft5336/*.c"
+"${TP_DIR}/framework-stm32cubeh7/Drivers/BSP/Components/is42s32800j/*.c"
+"${TP_DIR}/framework-stm32cubeh7/Drivers/BSP/Components/stmpe811/*.c"
+"${TP_DIR}/framework-stm32cubeh7/Drivers/BSP/Components/ov9655/*.c"
+"${TP_DIR}/framework-stm32cubeh7/Drivers/BSP/Components/wm8994/*.c"
+"${TP_DIR}/framework-stm32cubeh7/Drivers/BSP/Components/exc7200/*.c"
+"${TP_DIR}/framework-stm32cubeh7/Drivers/BSP/Adafruit_Shield/*.c"
+
+"${TP_DIR}/framework-stm32cubeh7/Middlewares/ST/STM32_USB_Device_Library/Class/*/Src/*.c"
+
+"${TP_DIR}/framework-stm32cubeh7/Utilities/Common/*.c"
+"${TP_DIR}/framework-stm32cubeh7/Utilities/JPEG/*.c"
+"${TP_DIR}/framework-stm32cubeh7/Utilities/Fonts/*.c"
+"${TP_DIR}/framework-stm32cubeh7/Utilities/lcd/*.c"
+"${TP_DIR}/framework-stm32cubeh7/Utilities/Lcd_Trace/*.c"
+"${TP_DIR}/framework-stm32cubeh7/Utilities/CPU/*.c"
+"${TP_DIR}/framework-stm32cubeh7/Utilities/ResourcesManager/*.c"
+)
+
+target_sources(stm32duino PUBLIC 
+${SOURCE_FILES} 
+${HARDWARE_DIR}/boards/stm32/NUCLEO-L476RG/Src/clock_config.c
+)
